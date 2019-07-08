c32f700898545b4ad1065d78a7dba37f1590a14e
david damilola <daviddamilola20@gmail.com>
2019-07-08 14:17:51 -0700

    feature(signup): add sign up endpoint[Finishes #167101532]

--- dist/routes/api/routes.js
+++ dist/routes/api/routes.js
@@ -7,15 +7,26 @@ exports["default"] = void 0;
 
 var _express = _interopRequireDefault(require("express"));
 
+var _users = _interopRequireDefault(require("../../controllers/users"));
+
+var _validate = _interopRequireDefault(require("../../middlewares/validate"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var validateEmail = _validate["default"].validateEmail,
+    validateFirstName = _validate["default"].validateFirstName,
+    validateLastName = _validate["default"].validateLastName,
+    validatePassword = _validate["default"].validatePassword;
+var validateSignUp = [validateEmail, validateFirstName, validateLastName, validatePassword];
 
 var router = _express["default"].Router();
 
-router.get("/", function (req, res) {
+router.get('/', function (req, res) {
   res.status(200).json({
-    status: "success",
-    message: "you have reached the api route successfully"
+    status: 'success',
+    message: 'you have reached the api route successfully'
   });
 });
+router.get('/auth/signup', _users["default"].welcomeSignUp).post('/auth/signup', validateSignUp, _users["default"].signUp);
 var _default = router;
 exports["default"] = _default;
--- dist/test/test.test.js
+++ dist/test/test.test.js
@@ -2,21 +2,21 @@
 
 var _chai = require("chai");
 
-var _server = _interopRequireDefault(require("../server"));
-
 var _supertest = _interopRequireDefault(require("supertest"));
+
+var _server = _interopRequireDefault(require("../server"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-describe("server set up", function () {
-  it(" should send all request routes through the Api route", function (done) {
-    (0, _supertest["default"])(_server["default"]).get("/api/v1/").end(function (err, res) {
+describe('server set up', function () {
+  it(' should send all request routes through the Api route', function (done) {
+    (0, _supertest["default"])(_server["default"]).get('/api/v1/').end(function (err, res) {
       (0, _chai.expect)(res.status).to.be.equal(200);
       done();
     });
   });
-  it("should catch routes that are not found", function (done) {
-    (0, _supertest["default"])(_server["default"]).get("/api/v1/unexistent_route").end(function (err, res) {
+  it('should catch routes that are not found', function (done) {
+    (0, _supertest["default"])(_server["default"]).get('/api/v1/unexistent_route').end(function (err, res) {
       (0, _chai.expect)(res.status).to.be.equal(404);
       done();
     });
