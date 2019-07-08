b6a9e7a117fcc2d049f7f949f79ff3c8ff047e55
david damilola <daviddamilola20@gmail.com>
2019-07-08 14:17:51 -0700

    feature(signup): add sign up endpoint[Finishes #167101532]

--- dist/server.js
+++ dist/server.js
@@ -7,9 +7,13 @@ exports["default"] = void 0;
 
 var _express = _interopRequireDefault(require("express"));
 
+var _expressValidator = _interopRequireDefault(require("express-validator"));
+
 var _dotenv = _interopRequireDefault(require("dotenv"));
 
 var _bodyParser = _interopRequireDefault(require("body-parser"));
+
+var _debug = _interopRequireDefault(require("debug"));
 
 var _routes = _interopRequireDefault(require("./routes/api/routes"));
 
@@ -21,17 +25,18 @@ _dotenv["default"].config();
 
 var errResponse = _utils["default"].errResponse;
 var app = (0, _express["default"])();
-var port = process.env.PORT || 5000;
+app.set('port', process.env.PORT || 2000);
 app.use(_bodyParser["default"].urlencoded({
   extended: false
 }));
 app.use(_bodyParser["default"].json());
+app.use((0, _expressValidator["default"])());
 app.use('/api/v1', _routes["default"]);
 app.all('*', function (req, res) {
   errResponse(res, 404, 'this page does not exist');
 });
-app.listen(port, function () {
-  app.listen(port, console.log("App listening on port ".concat(port)));
+app.listen(app.get('port'), function () {
+  (0, _debug["default"])('server/debug')("Express started on http://localhost:".concat(app.get('port'), "'; press Ctrl-C to terminate."));
 });
 var _default = app;
 exports["default"] = _default;
