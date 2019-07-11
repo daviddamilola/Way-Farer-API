1b4904025bed7e60ff85adf6c99ef3d6b165ee7b
david damilola <daviddamilola20@gmail.com>
2019-07-11 06:06:59 -0700

    feature(view-all-trips):add enpoint for admin and users to view all trips[Finishes #167222324]

--- src/controllers/trip.js
+++ src/controllers/trip.js
@@ -3,8 +3,10 @@ import Trip from '../models/Trip';
 import Utils from '../utils/utils';
 
 
-const { insert, response, errResponse } = Utils;
-class trips {
+const {
+  insert, response, errResponse, selectWhere,
+} = Utils;
+class Trips {
   static async createTrip(req, res) {
     const {
       bus_id, origin, destination, fare, status,
@@ -29,7 +31,30 @@ class trips {
       return errResponse(res, 500, 'an error occured please try again later');
     }
   }
+
+  static async viewTrips(req, res) {
+    try {
+      const rows = await selectWhere('trip', 'id, bus_id, origin, destination, trip_date, fare', 'status= $1', ['active']);
+      const data = rows.map((trip) => {
+        const obj = {
+          trip_id: trip.id,
+          bus_id: trip.bus_id,
+          origin: trip.origin,
+          destination: trip.destination,
+          trip_date: trip.trip_Date,
+          fare: trip.fare.toFixed(2),
+        };
+        return obj;
+      });
+      if (rows.length < 1) {
+        return errResponse(res, 404, 'no trips are currently available');
+      }
+      return response(res, 200, data);
+    } catch (error) {
+      return errResponse(res, 500, 'an error occurred, try again later');
+    }
+  }
 }
 
 
-export default trips;
+export default Trips;
